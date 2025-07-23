3ccb55765e71491991a557eeb92463a7ed71558e
Jai Gupta <jaiwork04@gmail.com>
2025-07-23 10:58:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors    = require('cors');
+
+const app = express();
+
+// const corsOptions = {
+//   origin: 'https://vitalcare-connect.vercel.app',
+// };
+
+app.use(express.json());
+app.use(cors());
+
+
+// GET API
+
+app.get('/api/test', async (req, res) => {
+
+  res.send('Hello World!');
+
+});
+
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
+
+module.exports = app;
